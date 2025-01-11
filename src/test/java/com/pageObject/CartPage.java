2ec441976b12e30618bd18fa7e7478898621f8b8
Mithu Roy <contact.mithuroy@gmail.com>
2025-01-12 02:37:53 +0530

    Update and Automated Add to Cart

--- src/test/java/com/pageObject/CartPage.java
+++ src/test/java/com/pageObject/CartPage.java
@@ -1,5 +1,126 @@
 package com.pageObject;
 
-public class CartPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
+public class CartPage {
+	WebDriver driver;
+
+	// Predefined card details
+	private String cardName = "John Doe";
+	private String cardNumber = "4111111111111111";
+	private String cvc = "123";
+	private String expiryMonth = "12";
+	private String expiryYear = "2025";
+
+	// Constructor
+	public CartPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	// Locators
+
+	@FindBy(xpath ="(//div[@class='product-overlay'])[1]")
+	private WebElement product; //hover on product box
+
+	@FindBy(xpath ="//button[normalize-space()='Continue Shopping']")
+	private WebElement continueShopping;
+
+	@FindBy(xpath = "//div[@class='col-sm-9 padding-right']//div[2]//div[1]//div[1]//div[2]//div[1]//a[1]")
+	private WebElement addToCartButtons;
+
+	@FindBy(xpath = "//a[normalize-space()='Cart']")
+	private WebElement cartLink;
+
+	@FindBy(xpath = "//tbody/tr")
+	private List<WebElement> cartItems;
+
+	@FindBy(xpath = "//button[contains(@class, 'update-quantity')]")
+	private WebElement updateQuantityButton;
+
+	@FindBy(xpath = "//input[@name='quantity']")
+	private WebElement quantityInput;
+
+	@FindBy(xpath = "//a[contains(text(),'Proceed To Checkout')]")
+	private WebElement proceedToCheckoutButton;
+
+	@FindBy(xpath = "//textarea[@name='message']")
+	private WebElement commentBox;
+
+	@FindBy(xpath = "//a[normalize-space()='Place Order']")
+	private WebElement placeOrderButton;
+
+	@FindBy(xpath = "//input[@name='name_on_card']")
+	private WebElement nameOnCardInput;
+
+	@FindBy(xpath = "//input[@name='card_number']")
+	private WebElement cardNumberInput;
+
+	@FindBy(xpath = "//input[@name='cvc']")
+	private WebElement cvcInput;
+
+	@FindBy(xpath = "//input[@name='expiry_month']")
+	private WebElement expiryMonthInput;
+
+	@FindBy(xpath = "//input[@name='expiry_year']")
+	private WebElement expiryYearInput;
+
+	@FindBy(xpath = "(//button[normalize-space()='Pay and Confirm Order'])")
+	private WebElement confirmOrderButton;
+
+	@FindBy(xpath = "//a[normalize-space()='Download Invoice']")
+	private WebElement downloadInvoiceButton;
+
+	// Actions
+	public void addProductsToCart() throws InterruptedException {
+
+		Actions action = new Actions(driver);
+		action.moveToElement(product).perform(); // Action to hover over the product
+		addToCartButtons.click();
+		Thread.sleep(2000);
+		continueShopping.click();
+
+	}
+
+	public void navigateToCart() {
+		cartLink.click();
+	}
+
+	public int getCartItemsCount() {
+		return cartItems.size();
+	}
+
+	public void updateProductQuantity(String quantity) {
+		quantityInput.clear();
+		quantityInput.sendKeys(quantity);
+		updateQuantityButton.click();
+	}
+
+	public void proceedToCheckout() {
+		proceedToCheckoutButton.click();
+	}
+
+	public void addComment(String comment) {
+		commentBox.sendKeys(comment);
+	}
+
+	public void placeOrder() {
+		placeOrderButton.click();
+		nameOnCardInput.sendKeys(cardName);
+		cardNumberInput.sendKeys(cardNumber);
+		cvcInput.sendKeys(cvc);
+		expiryMonthInput.sendKeys(expiryMonth);
+		expiryYearInput.sendKeys(expiryYear);
+		confirmOrderButton.click();
+	}
+
+	public void downloadInvoice() {
+		downloadInvoiceButton.click();
+	}
 }
--- src/test/java/com/testCases/CartPageTestCases.java
+++ src/test/java/com/testCases/CartPageTestCases.java
@@ -1,5 +1,69 @@
 package com.testCases;
+import com.pageObject.CartPage;
+import com.pageObject.LoginPage;
+import com.testBase.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-public class CartPageTestCases {
+public class CartPageTestCases extends BaseClass {
+    CartPage cartPage;
+    LoginPage loginPage;
 
+    @BeforeClass
+    public void setupPageObject() {
+        cartPage = new CartPage(driver);
+        loginPage = new LoginPage(driver);
+    }
+
+    @Parameters("url")
+    @Test(priority = 1, description = "Login to the application")
+    public void testLogin(String url) {
+        driver.get(url);
+        boolean loginSuccess = loginPage.loginWithValidCredentials(); // Login with predefined email and password
+        Assert.assertTrue(loginSuccess, "Login failed!");
+        System.out.println("1. Login successful.");
+    }
+
+    @Test(priority = 2, description = "Add new products using 'Add to Cart' button")
+    public void testAddProductsToCart() throws InterruptedException {
+        cartPage.addProductsToCart();
+        cartPage.navigateToCart();
+        int cartItemCount = cartPage.getCartItemsCount();
+        if(cartItemCount !=0) {
+        	 System.out.println("2. Products added to cart successfully.");
+        }else {
+        	 Assert.assertTrue(false, "Cart item count mismatch!");
+        }
+    }
+
+    @Test(priority = 3, description = "Verify the 'Update Quantity' feature")
+    public void testUpdateQuantity() {
+        cartPage.updateProductQuantity("3");
+        int cartItemCount = cartPage.getCartItemsCount();
+        //Assert.assertTrue(cartItemCount > 0, "Cart items not found after quantity update.");
+        System.out.println("3. Quantity updated successfully.");
+    }
+
+    @Test(priority = 4, description = "Test 'Proceed to Checkout' button functionality")
+    public void testProceedToCheckout() {
+        cartPage.proceedToCheckout();
+        System.out.println("4. Proceeded to checkout successfully.");
+    }
+
+    @Test(priority = 5, description = "Validate total price calculation and place order")
+    public void testPlaceOrder() {
+        cartPage.addComment("Please deliver between 9 AM and 5 PM.");
+        cartPage.placeOrder();
+        String currentURL = driver.getCurrentUrl();
+		Assert.assertTrue(currentURL.contains("payment_done"), "Place Order failed.");
+        System.out.println("5. Order placed successfully.");
+    }
+
+    @Test(priority = 6, description = "Download the invoice of payment")
+    public void testDownloadInvoice() {
+        cartPage.downloadInvoice();
+        System.out.println("6. Invoice downloaded successfully.");
+    }
 }
