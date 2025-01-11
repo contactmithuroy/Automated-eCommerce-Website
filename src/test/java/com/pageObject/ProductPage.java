61ab15ff0555ffecd78cc9caf5937f8c3eb1630b
Mithu Roy <contact.mithuroy@gmail.com>
2025-01-11 15:14:15 +0530

    Update Product Details Implementation

--- src/test/java/com/pageObject/ProductPage.java
+++ src/test/java/com/pageObject/ProductPage.java
@@ -23,10 +23,50 @@ public class ProductPage {
     @FindBy(xpath ="//button[normalize-space()='Continue Shopping']")
 	private WebElement continueShopping;
     
+    @FindBy(xpath = "//input[@id='search_product']")
+    private WebElement searchInput;
+
+    @FindBy(xpath = "//button[@id='submit_search']")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//h2[@class='title text-center']")
+    private WebElement searchResultsTitle;
+    
+    @FindBy(xpath = "//a[normalize-space()='Women']")
+    private WebElement womenCategory;
+    
+    @FindBy(xpath="(//a[contains(text(),'Dress')])")
+    private WebElement womenCategoryChildList;
+
+    @FindBy(xpath = "//a[normalize-space()='Men']")
+    private WebElement menCategory;
+    
+    @FindBy(xpath="(//a[contains(text(),'Jeans')])")
+    private WebElement menCategoryChildList;
+
+    @FindBy(xpath = "//a[normalize-space()='Kids']")
+    private WebElement kidsCategory;
+    
+    @FindBy(xpath="(//a[contains(text(),'Tops & Shirts')])")
+    private WebElement kidsCategoryChildList;
+    
+    @FindBy(xpath="//a[@href='/brand_products/Polo']")
+    private WebElement poloBrand;
+    
+    @FindBy(xpath="//a[@href='/brand_products/H&M']")
+    private WebElement handmBrand;
+    
+    @FindBy(xpath="//a[@href='/brand_products/Biba']")
+    private WebElement bibaBrand;
+    
+    @FindBy(xpath="(//a[contains(text(),'View Product')])[1]")
+    private WebElement viewProduct;
+    
 	//Action
     public String getPageTitle(WebDriver driver) {
         return driver.getTitle();
     }
+    
     public void clickAddToProduct(WebDriver driver) throws InterruptedException {
     	Actions action = new Actions(driver);
         action.moveToElement(product).perform(); // Action to hover over the product
@@ -36,4 +76,38 @@ public class ProductPage {
     	continueShopping.click();
     	
     }
+    
+	public void searchProduct(String searchVal) {
+		 searchInput.sendKeys(searchVal);
+	        searchButton.click();
+	}
+	
+	public void selectCategory(String category) {
+        if (category.equalsIgnoreCase("Women")) {
+            womenCategory.click();
+            womenCategoryChildList.click();
+            } else if (category.equalsIgnoreCase("Men")) {
+            menCategory.click();
+            menCategoryChildList.click();
+        } else if (category.equalsIgnoreCase("Kids")) {
+            kidsCategory.click();
+            kidsCategoryChildList.click();
+        }
+    }
+	
+	public void brandOption(String brand) {
+		if(brand.equalsIgnoreCase("Polo")) {
+			poloBrand.click();
+		}else if(brand.equalsIgnoreCase("H&M")) {
+			handmBrand.click();
+			
+		}else if(brand.equalsIgnoreCase("Biba")) {
+			bibaBrand.click();
+		}
+	}
+	
+	public void viewProduct() {
+		viewProduct.click();
+	}
+	
 }
--- src/test/java/com/testCases/ProductPageTestCases.java
+++ src/test/java/com/testCases/ProductPageTestCases.java
@@ -30,4 +30,56 @@ public class ProductPageTestCases extends BaseClass {
         Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/products", "Products link navigation failed.");
         System.out.println("Verified Add to Cart functionality.");
     }
+    
+    @Test(priority = 3, description = "Test the search functionality on the product page")
+    public void testSearchFunctionality() {
+    	String searchVal = "Shirt";
+        productPage.searchProduct(searchVal); // Example product
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://automationexercise.com/products?search="+searchVal;
+        Assert.assertEquals(currentUrl,expectedUrl, "Search results did not match.");
+        System.out.println("Search functionality works as expected.");
+    }
+    
+    @Test(priority = 4, description = "Validate that category option (WOMEN, MEN, KIDS) works correctly")
+    public void testCategorySelection() {
+        productPage.selectCategory("Women");
+        Assert.assertTrue(driver.getTitle().contains("Automation Exercise"), "Women category navigation failed.");
+
+        driver.navigate().back();
+
+        productPage.selectCategory("Men");
+        Assert.assertTrue(driver.getTitle().contains("Automation Exercise"), "Men category navigation failed.");
+
+        driver.navigate().back();
+
+        productPage.selectCategory("Kids");
+        Assert.assertTrue(driver.getTitle().contains("Automation Exercise"), "Kids category navigation failed.");
+        
+        driver.navigate().back();
+        System.out.println("Category Selection functionality works as expected.");
+    }
+    
+    @Test(priority = 5, description = "Validate that brand option (Polo, H&M, Biba) works correctly")
+    public void testBranSelection() {
+        productPage.brandOption("Polo");
+        Assert.assertTrue(driver.getTitle().contains("Polo"), "Polo brand navigation failed.");
+
+        productPage.brandOption("H&M");
+        Assert.assertTrue(driver.getTitle().contains("H&M"), "H&M brand navigation failed.");
+        
+        productPage.brandOption("Biba");
+        Assert.assertTrue(driver.getTitle().contains("Biba"), "Biba brand navigation failed.");
+        
+        System.out.println("Brand Selection functionality works as expected.");
+    }
+    
+    @Test(priority = 6, description = "Validate that product details page works correctly")
+    public void productDetailsButton() {
+        productPage.viewProduct();
+        Assert.assertTrue(driver.getTitle().contains("Product Details"), "Product details navigation failed.");
+        driver.navigate().back();
+        System.out.println("Product Details page Selection functionality works as expected.");
+    }
+    
 }
